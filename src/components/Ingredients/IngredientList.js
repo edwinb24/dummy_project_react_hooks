f33fde01ebe7304a2934261bb9e3cfe8275f6af9
edwinb24 <edwin_b24@hotmail.com>
2020-06-28 14:53:00 -0500

    Practicing Hooks

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -7,10 +7,10 @@ const IngredientList = props => {
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
-            <span>{ig.title}</span>
-            <span>{ig.amount}x</span>
+        {props.ingredients.map(ingredient => (
+          <li key={ingredient.id} onClick={props.onRemoveItem.bind(this, ingredient.id)}>
+            <span>{ingredient.title}</span>
+            <span>{ingredient.amount}x</span>
           </li>
         ))}
       </ul>
